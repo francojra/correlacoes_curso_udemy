d44a0e4042038d86ab0c6d7a6e448e475469ab1c
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-07 22:26:32 -0300

    Add files via upload

--- correlacoes_Udemy.R
+++ correlacoes_Udemy.R
@@ -31,3 +31,36 @@ cor.test(Orange$age, Orange$circumference) # Padrão = Pearson
 cor(Orange$age, Orange$circumference, method = "pearson")
 cor.test(Orange$age, Orange$circumference, method = "kendall")
 cor.test(Orange$age, Orange$circumference, method = "spearman")
+
+# Correlações com múltiplas variáveis ------------------------------------------------------------------------------------------------------
+
+### Matriz de correlação
+
+# Carregar dados ---------------------------------------------------------------------------------------------------------------------------
+
+data("mtcars")
+View(mtcars)
+
+# Matriz de correlação ---------------------------------------------------------------------------------------------------------------------
+
+cor(mtcars)
+
+library(corrplot)
+?corrplot
+
+m <- cor(mtcars)
+View(m)
+
+### Tipos de matrizes
+
+corrplot(m) # Padrão com circulos
+corrplot(m, method = "color") # Cores
+corrplot(m, method = "ellipse") # Elipses
+corrplot(m, method = "shade") # Tons
+corrplot(m, method = "number") # Números dos valores das correlações
+
+### Posições
+
+corrplot(m, type = "upper") 
+corrplot(m, type = "lower") 
+corrplot(m, type = "lower", method = "number")
